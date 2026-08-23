301b01b53621eaadaf2a3b6c398e347e73755a7e
0105152627 <0105152627@users.noreply.github.com>
2026-08-23 23:22:23 +0800

    Remove manual curation labels

--- static-api.js
+++ static-api.js
@@ -3,7 +3,7 @@
   'use strict';
   const nativeFetch = window.fetch.bind(window);
   const base = new URL('./', document.currentScript.src);
-  const dataVersion = '20260823-curated-chains-v1';
+  const dataVersion = '20260823-curated-chains-v2';
   const jsonCache = new Map();
   const bucketCache = new Map();
   const scanCache = new Map();
